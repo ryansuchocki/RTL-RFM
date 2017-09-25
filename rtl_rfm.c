de60adf528ba14c63d7592066a92f80755ddf800
ryansuchocki <ryan@ryansuchocki.co.uk>
2017-09-25 22:49:24 +0100

    autopush

--- rtl_rfm.c
+++ rtl_rfm.c
@@ -81,7 +81,7 @@ int main (int argc, char **argv) {
 
         for (uint16_t i = 0; i < data_len; i++) {
 
-        	fprintf(stderr, "@%i\t", dataI[i]);
+        	fprintf(stderr, "@%i\t", dataQ[i]);
 
 			int8_t bit = fsk_decode(fm_demod(dataI[i], dataQ[i]), fm_magnitude);
 			if (bit >= 0) rfm_decode(bit);
