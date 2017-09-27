8f0f8cb59bd94c097e81bc19e035e7cf67878da7
ryansuchocki <ryan@ryansuchocki.co.uk>
2017-09-27 12:19:16 +0100

    autopush

--- reader_thread.c
+++ reader_thread.c
@@ -66,7 +66,7 @@ void rtlsdr_callback(unsigned char *buf, uint32_t len, void *ctx) {
     fprintf(stderr, ",");
 
     data_ready = 1;
-    pthread_cond_signal(&data_cond);
+    fprintf(stderr, "<%i>", pthread_cond_signal(&data_cond));
     pthread_mutex_unlock(&data_mutex);
 }
 
