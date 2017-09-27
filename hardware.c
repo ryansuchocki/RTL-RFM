c5bc7c92b0baf32d83c37b741f2cdf899b90ad76
ryansuchocki <ryan@ryansuchocki.co.uk>
2017-09-27 21:35:57 +0100

    autopush

--- hardware.c
+++ hardware.c
@@ -35,7 +35,7 @@ int hw_init() {
 	if (rtlsdr_set_sample_rate(dev, BIGSAMPLERATE) < 0) return -3;
 	if (rtlsdr_set_tuner_gain_mode(dev, 1) < 0) return -4;
 	if (rtlsdr_set_tuner_gain(dev, nearest_gain(dev, 500)) < 0) return -5;
-	if (rtlsdr_set_freq_correction(dev, ppm_error) < 0) return -6;
+	if (rtlsdr_set_freq_correction(dev, ppm) < 0) return -6;
 	if (rtlsdr_reset_buffer(dev) < 0) return -7;
 
 	return 0;
