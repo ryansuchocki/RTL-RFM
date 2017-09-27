e32706765edaf35831edf2d2164746dd7da7f3b4
ryansuchocki <ryan@ryansuchocki.co.uk>
2017-09-27 21:35:33 +0100

    autopush

--- hardware.c
+++ hardware.c
@@ -1,15 +1,9 @@
 #include "rtl_rfm.h"
 #include "hardware.h"
 
-
-
 rtlsdr_dev_t *dev = NULL;
 
-
-
-
-int nearest_gain(rtlsdr_dev_t *dev, int target_gain)
-{
+int nearest_gain(rtlsdr_dev_t *dev, int target_gain) {
 	int i, r, err1, err2, count, nearest;
 	int* gains;
 	r = rtlsdr_set_tuner_gain_mode(dev, 1);
@@ -35,82 +29,14 @@ int nearest_gain(rtlsdr_dev_t *dev, int target_gain)
 	return nearest;
 }
 
-int verbose_set_frequency(rtlsdr_dev_t *dev, uint32_t frequency)
-{
-	int r;
-	r = rtlsdr_set_center_freq(dev, frequency);
-	if (r < 0) {
-		fprintf(stderr, "WARNING: Failed to set center freq.\n");
-	} else {
-		fprintf(stderr, "Tuned to %u Hz.\n", frequency);
-	}
-	return r;
-}
-
-int verbose_set_sample_rate(rtlsdr_dev_t *dev, uint32_t samp_rate)
-{
-	int r;
-	r = rtlsdr_set_sample_rate(dev, samp_rate);
-	if (r < 0) {
-		fprintf(stderr, "WARNING: Failed to set sample rate.\n");
-	} else {
-		fprintf(stderr, "Sampling at %u S/s.\n", samp_rate);
-	}
-	return r;
-}
-
-
-
-int verbose_gain_set(rtlsdr_dev_t *dev, int gain)
-{
-	int r;
-	r = rtlsdr_set_tuner_gain_mode(dev, 1);
-	if (r < 0) {
-		fprintf(stderr, "WARNING: Failed to enable manual gain.\n");
-		return r;
-	}
-	r = rtlsdr_set_tuner_gain(dev, gain);
-	if (r != 0) {
-		fprintf(stderr, "WARNING: Failed to set tuner gain.\n");
-	} else {
-		fprintf(stderr, "Tuner gain set to %0.2f dB.\n", gain/10.0);
-	}
-	return r;
-}
-
-int verbose_ppm_set(rtlsdr_dev_t *dev, int ppm_error)
-{
-	int r;
-	if (ppm_error == 0) {
-		return 0;}
-	r = rtlsdr_set_freq_correction(dev, ppm_error);
-	if (r < 0) {
-		fprintf(stderr, "WARNING: Failed to set ppm error.\n");
-	} else {
-		fprintf(stderr, "Tuner error set to %i ppm.\n", ppm_error);
-	}
-	return r;
-}
-
-int verbose_reset_buffer(rtlsdr_dev_t *dev)
-{
-	int r;
-	r = rtlsdr_reset_buffer(dev);
-	if (r < 0) {
-		fprintf(stderr, "WARNING: Failed to reset buffers.\n");}
-	return r;
-}
-
-
 int hw_init() {
-	if (rtlsdr_open(&dev, 0) < 0) {
-		fprintf(stderr, "Failed to open rtlsdr device #%d.\n", 0);
-		return -1;
-	}
-	
-	verbose_set_frequency(dev, freq); // Set freq before sample rate to avoid "PLL NOT LOCKED"
-	verbose_set_sample_rate(dev, BIGSAMPLERATE);
-	verbose_gain_set(dev, nearest_gain(dev, 500));
-	verbose_ppm_set(dev, ppm);
-	verbose_reset_buffer(dev);
+	if (rtlsdr_open(&dev, 0) < 0) return -1;
+	if (rtlsdr_set_center_freq(dev, freq) < 0) return -2; // Set freq before sample rate to avoid "PLL NOT LOCKED"
+	if (rtlsdr_set_sample_rate(dev, BIGSAMPLERATE) < 0) return -3;
+	if (rtlsdr_set_tuner_gain_mode(dev, 1) < 0) return -4;
+	if (rtlsdr_set_tuner_gain(dev, nearest_gain(dev, 500)) < 0) return -5;
+	if (rtlsdr_set_freq_correction(dev, ppm_error) < 0) return -6;
+	if (rtlsdr_reset_buffer(dev) < 0) return -7;
+
+	return 0;
 }
